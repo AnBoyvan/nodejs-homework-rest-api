367fc234a1ddb46c6d88b2714e68dde47d59044b
AnBoyvan <andrew.boyvan1988@gmail.com>
2023-04-25 21:25:42 +0300

    done

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const validateBody = require("./validateBody");
 const resizeImg = require("./resizeImg");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   ctrlWrapper,
   validateBody,
   resizeImg,
+  sendEmail,
 };
